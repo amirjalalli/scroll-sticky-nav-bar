7bbc19069fb88db0e7bf1640aaeff4022bd562e2
amirjalalli <87828297+amirjalalli@users.noreply.github.com>
2021-12-23 16:35:24 +0330

    scroll-sticky-nav-bar

---
+++ js.js
@@ -0,0 +1,15 @@
+const mainNav = document.getElementById('mainNav')
+const logoImg = document.querySelector('img')
+
+document.addEventListener('scroll',function () {
+    // console.log(document.documentElement.scrollTop)
+    if (document.documentElement.scrollTop > 0){
+        mainNav.classList.add('bg-black')
+        mainNav.classList.add('txt-white')
+        logoImg.style.height = '64px'
+    } else {
+        mainNav.classList.remove('bg-black')
+        mainNav.classList.remove('txt-white')
+        logoImg.style.height = '84px'
+    }
+})
